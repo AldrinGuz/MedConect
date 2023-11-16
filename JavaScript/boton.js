66a6dfe5c9b591a4e8aa79ee1017141a0543edb0
Aldrin Guzman Figueroa <abgf3@alu.ua.es>
2023-11-16 21:08:31 +0100

    CASI todo listo.

--- JavaScript/boton.js
+++ JavaScript/boton.js
@@ -5,3 +5,26 @@ function setModo(hoja) {
 		console.log(document.URL);
 	}
 }
+function openVideo(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+function desplegar(id){
+
+    let contenido= document.getElementsByClassName(id);
+    if(contenido.style.display=="none"){
+        contenido.style.display = "block";
+    }
+    else{
+        contenido.style.display = "none";
+    }
+}
